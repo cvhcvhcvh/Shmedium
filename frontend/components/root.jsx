c0fd2e6a0fa6ccb78d3fdb8c35235600af1074e6
cvhcvhcvh <77473921+cvhcvhcvh@users.noreply.github.com>
2021-07-03 14:52:01 -0500

    made reducers

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Root = () => {
   return(
-  <h1>maybe?</h1>)
+  <h1>Welcome to the Jungle</h1>)
 }
 
 export default Root
--- frontend/daddyfile.jsx
+++ frontend/daddyfile.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Root from './components/root';
-import { login, logout } from "./util/session_api_util";
+import { signup, login, logout } from "./actions/session_actions";
+import configureStore from "./store/store";
+
 
 document.addEventListener("DOMContentLoaded", () => {
   window.login = login;
   window.logout = logout;
+  window.signup = signup;
+  window.store = configureStore;
+  
+  const store = configureStore();
   const root = document.getElementById("root");
-  ReactDOM.render(<Root/>, root);
+  ReactDOM.render(<Root store={store}/>, root);
 });
 

33e3b1a0dd10fc669422de900be491aeb126cd96
cvhcvhcvh <77473921+cvhcvhcvh@users.noreply.github.com>
2021-07-02 23:19:18 -0500

    completed users_controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,22 @@
-class Api::UsersController < ApplicationController
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+      login!(@user)
+      # redirect_to poems_url
+    else
+      flash[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:full_name, :email)
+  end
 end

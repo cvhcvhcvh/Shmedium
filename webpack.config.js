ab63538ae2bbc0d95c4ad69056ceaaad9146adab
cvhcvhcvh <77473921+cvhcvhcvh@users.noreply.github.com>
2021-09-21 11:15:24 -0500

    test, all the changes

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 var path = require('path');
 
 module.exports = {
+  mode: 'production',
   entry: './frontend/daddyfile.jsx',
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),

7dffef42d69ba3f8ebd4c277431cb1689399e2ce
cvhcvhcvh <77473921+cvhcvhcvh@users.noreply.github.com>
2021-09-21 12:29:10 -0500

    import openModal

--- frontend/components/response/response_form.jsx
+++ frontend/components/response/response_form.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { openModal } from '../../actions/modal_actions';
 import ResponseIndexItem from './response_index_item';
 
 class ResponseForm extends React.Component {
